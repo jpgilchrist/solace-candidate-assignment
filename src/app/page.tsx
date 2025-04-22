2a741df4ceba660d6bf60f99575e0ad6045d94dc
jpgilchrist <james.p.gilchrist@gmail.com>
2025-04-22 13:23:21 -0400

    ref #6, #3: implements shadcn/ui and tanstack/data-table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,56 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
-import { AdvocateResponse } from "./common-types";
+import { Advocate, AdvocateResponse } from "./common-types";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/data-table";
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { DataTableColumnHeader } from "@/components/data-table-header";
+import { DataTablePagination } from "@/components/data-table-pagination";
+
+const columnHelper = createColumnHelper<Advocate>();
+const defaultColumns = [
+  columnHelper.accessor("firstName", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="First Name" />
+    ),
+  }),
+  columnHelper.accessor("lastName", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Last Name" />
+    ),
+  }),
+  columnHelper.accessor("city", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="City" />
+    ),
+  }),
+  columnHelper.accessor("degree", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Degree" />
+    ),
+  }),
+  columnHelper.accessor("specialties", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Specialties" />
+    ),
+  }),
+  columnHelper.accessor("yearsOfExperience", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Experience" />
+    ),
+  }),
+  columnHelper.accessor("phoneNumber", {
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Phone" />
+    ),
+  }),
+];
 
 export default function Home() {
   const [globalFilter, setGlobalFilter] = useState<string>("");
@@ -28,57 +77,28 @@ export default function Home() {
     },
   });
 
+  const table = useReactTable({
+    data: query.data?.data ?? [],
+    columns: defaultColumns,
+    getCoreRowModel: getCoreRowModel(),
+    enableRowSelection: false,
+  });
+
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
+    <main className="p-6 h-full flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Solace Advocates</h1>
+
       <div>
         <p>Search</p>
         <p>
           Searching for: <span id="search-term"></span>
         </p>
-        <input onChange={(e) => setGlobalFilter(e.target.value)} />
-        <button onClick={() => setGlobalFilter("")}>Reset Search</button>
+        <Input onChange={(e) => setGlobalFilter(e.target.value)} />
+        <Button onClick={() => setGlobalFilter("")}>Reset Search</Button>
       </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
-        </thead>
-        <tbody>
-          {query.isLoading ? (
-            <span>loading...</span>
-          ) : query.data?.data.length ? (
-            query.data.data.map((advocate) => {
-              return (
-                <tr>
-                  <td>{advocate.firstName}</td>
-                  <td>{advocate.lastName}</td>
-                  <td>{advocate.city}</td>
-                  <td>{advocate.degree}</td>
-                  <td>
-                    {advocate.specialties.map((s) => (
-                      <div>{s}</div>
-                    ))}
-                  </td>
-                  <td>{advocate.yearsOfExperience}</td>
-                  <td>{advocate.phoneNumber}</td>
-                </tr>
-              );
-            })
-          ) : (
-            <span>no results...</span>
-          )}
-        </tbody>
-      </table>
+
+      <DataTable table={table} loading={query.isLoading} />
+      <DataTablePagination table={table} />
     </main>
   );
 }
