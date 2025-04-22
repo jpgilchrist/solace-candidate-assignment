d9735c265cc22bd1fd024aba8e29904a6c37c47c
James Gilchrist <james.p.gilchrist@gmail.com>
2025-04-22 13:51:39 -0400

    ref #8: modify db column types (#9)
    Closes #8

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,23 +1,22 @@
 import { sql } from "drizzle-orm";
 import {
-  pgTable,
   integer,
-  text,
   jsonb,
+  pgTable,
   serial,
   timestamp,
-  bigint,
+  varchar
 } from "drizzle-orm/pg-core";
 
 const advocates = pgTable("advocates", {
   id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
-  city: text("city").notNull(),
-  degree: text("degree").notNull(),
+  firstName: varchar("first_name", { length: 100 }).notNull(),
+  lastName: varchar("last_name", { length: 100 }).notNull(),
+  city: varchar("city", { length: 255 }).notNull(),
+  degree: varchar("degree", { length: 10 }).notNull(),
   specialties: jsonb("payload").default([]).notNull(),
   yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
+  phoneNumber: varchar("phone_number", { length: 15 }).notNull(),
   createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
 });
 
