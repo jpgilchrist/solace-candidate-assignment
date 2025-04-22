46d9b594e4fe110809c2300a0829ebd85170bfe7
jpgilchrist <james.p.gilchrist@gmail.com>
2025-04-22 12:40:07 -0400

    ref #4: add search term implementation

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,8 +1,29 @@
+import { NextRequest } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
+import { ilike, or, sql } from "drizzle-orm";
 
-export async function GET() {
-  const data = await db.select().from(advocates);
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+
+  const search = url.searchParams.get("search");
+  const ilikeValue = `%${search}%`;
+
+  const data = await db
+    .select()
+    .from(advocates)
+    .where(
+      search
+        ? or(
+            ilike(advocates.firstName, ilikeValue),
+            ilike(advocates.lastName, ilikeValue),
+            ilike(advocates.city, ilikeValue),
+            ilike(advocates.degree, ilikeValue),
+            sql`${advocates.specialties}::text ILIKE ${ilikeValue}`,
+            ilike(advocates.yearsOfExperience, ilikeValue)
+          )
+        : undefined
+    );
 
   return Response.json({ data });
 }
